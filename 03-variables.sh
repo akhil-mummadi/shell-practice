481e19d106b3aee3c7dc99a38cb6c5e15ad3efb2
Akhil Mummadi <akhilsp234@gmail.com>
2026-01-28 15:17:15 +0530

    added something

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Akhil
+PERSON2=Bhargav
+
+echo "$PERSON1:: Hello $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine."
+
+echo "$PERSON1:: Hello $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine."
