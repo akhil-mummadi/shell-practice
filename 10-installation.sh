23f077960157ddc17ca8436cedd79e11bb895b47
Akhil Mummadi <akhilsp234@gmail.com>
2026-01-28 16:47:28 +0530

    added something

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+echo "Installing nginx"
 dnf install nginx -y
